96a7074f360d7c19be1b69597effb74ecaf6ba84
Deflaktor <deflaktor@gmail.com>
2021-04-24 22:23:35 +0200

    Add fix for easy mode maps crashing in the standard mapset

--- lib/dolio/rulesettable.cpp
+++ lib/dolio/rulesettable.cpp
@@ -26,7 +26,6 @@ void RuleSetTable::writeAsm(QDataStream &stream, const AddressMapper &addressMap
     stream.device()->seek(addressMapper.toFileAddress(ruleSetFromMapRoutine));
     auto insts = writeRuleSetFromMapRoutine(addressMapper, ruleSetFromMapRoutine); // re-write the routine again since now we know where it is located in the main dol
     for (quint32 inst: qAsConst(insts)) stream << inst;
-
     quint32 virtualPos = addressMapper.boomStreetToStandard(0x8007e13c);
     stream.device()->seek(addressMapper.toFileAddress(virtualPos));
     // lha r3,0x3c(r30)  -> bl ruleSetFromMapRoutine
@@ -38,6 +37,21 @@ void RuleSetTable::writeAsm(QDataStream &stream, const AddressMapper &addressMap
     // stw r25,0x53f4(r29) -> lha r0,0x28(r30)
     stream << PowerPcAsm::lha(0, 0x28, 30);
 
+    /*
+     * Bug Fix: The monopoly check uses the GameRule flag from GameSelectInfo instead of the global variable. However, the GameRuleFlag is now repurposed as the mapset and as such
+     * independent from the actual game rules. As both standard and easy maps can be in either mapsets, we need to query here the easyrule global flag instead of the GameRuleFlag
+     * from GameSelectInfo.
+     */
+    quint32 returnAddr = addressMapper.boomStreetToStandard(0x801c4690);
+    auto getRuleSetRoutine = allocate(writeGetRuleSetRoutine(0, returnAddr), "writeRuleSetFromMapRoutine");
+    stream.device()->seek(addressMapper.toFileAddress(getRuleSetRoutine));
+    insts = writeGetRuleSetRoutine(getRuleSetRoutine, returnAddr); // re-write the routine again since now we know where it is located in the main dol
+    for (quint32 inst: qAsConst(insts)) stream << inst;
+    virtualPos = addressMapper.boomStreetToStandard(0x801c468c);
+    stream.device()->seek(addressMapper.toFileAddress(virtualPos));
+    // lha r3,0x3c(r30)  -> bl ruleSetFromMapRoutine
+    stream << PowerPcAsm::bl(virtualPos, getRuleSetRoutine);
+
     // -- Always enable the buttons for ON/OFF negotiations and "Add vacant plots"
     // easy mode negotiations
     stream.device()->seek(addressMapper.boomToFileAddress(0x801e237c)); stream << PowerPcAsm::nop();
@@ -48,6 +62,9 @@ void RuleSetTable::writeAsm(QDataStream &stream, const AddressMapper &addressMap
     // standard mode vacant plots
     stream.device()->seek(addressMapper.boomToFileAddress(0x801e2544)); stream << PowerPcAsm::nop();
     stream.device()->seek(addressMapper.boomToFileAddress(0x801e2554)); stream << PowerPcAsm::nop();
+
+    // enable vacant plots placement in easy mode as well
+    stream.device()->seek(addressMapper.boomToFileAddress(0x800ccd20)); stream << PowerPcAsm::nop();
 
 }
 
@@ -85,15 +102,30 @@ bool RuleSetTable::readIsVanilla(QDataStream &stream, const AddressMapper &addre
 
 QVector<quint32> RuleSetTable::writeRuleSetFromMapRoutine(const AddressMapper &addressMapper, quint32 routineStartAddress) {
     quint32 Game_GetRuleFlag = addressMapper.boomStreetToStandard(0x801cca98);
+    quint32 GetGameSelectInfo = addressMapper.boomStreetToStandard(0x800113f0);
 
+    // precondition: r7 is unused
     // precondition: r24 is mapId
     // precondition: r25 is global rule set which we are gonna use to store the linkreturn
     return {
         PowerPcAsm::mflr(25),
-        PowerPcAsm::mr(3, 24),                                             // r3 <- r24
+        PowerPcAsm::mr(3, 24),                                                   // r3 <- r24
         PowerPcAsm::bl(routineStartAddress, 2 /*asm.Count*/, Game_GetRuleFlag),  // r3 <- bl Game_GetRuleFlag(r3)
-        PowerPcAsm::stw(3, 0x53f4, 29),                                    // gameRule <- r3
+        PowerPcAsm::stw(3, 0x53f4, 29),                                          // gameRule <- r3
+        //PowerPcAsm::mr(7, 3),                                                    // r7 <- r3
+        //PowerPcAsm::bl(routineStartAddress, 5 /*asm.Count*/, GetGameSelectInfo), // r3 <- bl GetGameSelectInfo()
+        //PowerPcAsm::stb(7, 0x226, 3),                                            // GetGameSelectInfo.easyRules = gameRule
         PowerPcAsm::mtlr(25),
-        PowerPcAsm::blr()                                                 // return
+        PowerPcAsm::blr()                                                        // return
     };
 }
+
+QVector<quint32> RuleSetTable::writeGetRuleSetRoutine(quint32 routineStartAddress, quint32 routineReturnAddress) {
+    return {
+        PowerPcAsm::lis(28, 0x8055),             // |
+        PowerPcAsm::addi(28, 28, 0x240f),        // / load address of global easy variable
+        PowerPcAsm::lbz(28, 0, 28),              // r28 <- easyMode
+        PowerPcAsm::b(routineStartAddress, 3/*asm.count*/, routineReturnAddress)
+    };
+}
+
