45d5fc8f1bebb4ee49401fd68db4d305e547a3a4
Deflaktor <deflaktor@gmail.com>
2021-10-14 21:33:00 +0200

    Implement export command

--- maincli.cpp
+++ maincli.cpp
@@ -11,10 +11,11 @@
 namespace maincli {
 
 // From: https://github.com/benlau/asyncfuture/issues/11
+
 template <typename T>
- inline void await(QFuture<T> future, int timeout = -1) {
+inline T await(QFuture<T> future, int timeout = -1) {
      if (future.isFinished()) {
-         return;
+         return future.result();
      }
 
      QFutureWatcher<T> watcher;
@@ -27,7 +28,26 @@ template <typename T>
 
      QObject::connect(&watcher, SIGNAL(finished()), &loop, SLOT(quit()));
      loop.exec();
- }
+
+     return future.result();
+}
+
+inline void await(QFuture<void> future, int timeout = -1) {
+     if (future.isFinished()) {
+         return;
+     }
+
+     QFutureWatcher<void> watcher;
+     watcher.setFuture(future);
+     QEventLoop loop;
+
+     if (timeout > 0) {
+         QTimer::singleShot(timeout, &loop, &QEventLoop::quit);
+     }
+
+     QObject::connect(&watcher, SIGNAL(finished()), &loop, SLOT(quit()));
+     loop.exec();
+}
 
 void setupSubcommand(QCommandLineParser& parser, QString name, QString description) {
     parser.clearPositionalArguments();
@@ -99,8 +119,6 @@ void run(QStringList arguments)
     QSharedPointer<QTextStream> coutp(parser.isSet(quietOption)? new QTextStream(&b): new QTextStream(stdout));
     QTextStream& cout = *coutp;
 
-    QTextStream cerr(stderr);
-
     const QStringList args = parser.positionalArguments();
     const QString command = args.isEmpty() ? QString() : args.first();
 
@@ -109,10 +127,11 @@ void run(QStringList arguments)
         cout << parser.helpText();
         cout << commandsDescription;
     } else if (command == "extract") {
+        // --- extract ---
         setupSubcommand(parser, "extract", "Extract a Fortune Street game disc image to a directory. This is equivalent to\n  wit copy --psel data --preserve --fst source extractDir");
 
         parser.addPositionalArgument("source", "Source Fortune Street game disc image (.wbfs, .iso).", "extract <source>");
-        parser.addPositionalArgument("extractDir", "Target directory to be the container of the Fortune Street game disc. [default = <workingDirectory>/<sourceFilename>]", "[extractDir]");
+        parser.addPositionalArgument("extractDir", "Target directory to be the container of the Fortune Street game disc.\n[default = <workingDirectory>/<sourceFilename>]", "[extractDir]");
         parser.addOption(forceOption);
 
         parser.process(arguments);
@@ -122,26 +141,99 @@ void run(QStringList arguments)
         } else {
             const QString source = args.at(1);
             QFileInfo sourceFileInfo(source);
+            if(!sourceFileInfo.exists()) {
+                cout << source << " does not exist.\n";
+                QCoreApplication::exit(1); return;
+            } else if(!sourceFileInfo.isFile()) {
+                cout << source << " is not a file.\n";
+                QCoreApplication::exit(1); return;
+            }
             const QString target = args.size() >= 3? args.at(2) : QDir::current().filePath(sourceFileInfo.baseName());
-            QFileInfo targetFileInfo(target);
+            const QDir targetDir(target);
             cout << "Extracting " << source << " to " << target << "...\n";
-            if(targetFileInfo.exists()) {
+            if(targetDir.exists()) {
                 if(parser.isSet(forceOption)) {
                     cout << "Overwriting " << target << "...\n";
                 } else {
                     cout << "Cannot extract as " << target << " already exists. Use force option to overwrite.\n";
-                    return;
+                    QCoreApplication::exit(1); return;
                 }
+            } else {
+                targetDir.mkpath("");
             }
             cout.flush();
             await(ExeWrapper::extractWbfsIso(source, target));
-            cout << "done!";
         }
-    } else if (command == "close") {
-
     } else if (command == "export") {
+        // --- export ---
+        setupSubcommand(parser, "export", "Export one or several map descriptor files (*.yaml) from a Fortune Street game directory.");
 
+        parser.addPositionalArgument("sourceDir", "Source Fortune Street game directory.", "export <sourceDir>");
+        parser.addPositionalArgument("targetDir", "Target directory to be the container of the exported map descriptor.\n[default = <workingDirectory>/<mapName>.yaml]", "[targetDir]");
+        parser.addOption(allOption);
+        parser.addOption(forceOption);
+        parser.addOption(mapIdOption);
+        parser.addOption(internalNamesOption);
+
+        parser.process(arguments);
+        const QStringList args = parser.positionalArguments();
+        if(parser.isSet(helpOption) || args.size() < 2) {
+            cout << '\n' << parser.helpText();
+        } else {
+            const QString source = args.at(1);
+            const QDir sourceDir(source);
+            if(!sourceDir.exists()) {
+                cout << source << " does not exist.";
+                QCoreApplication::exit(1); return;
+            }
+            const QString target = args.size() >= 3? args.at(2) : QDir::current().path();
+            QDir targetDir(target);
+            if(!targetDir.exists()) {
+                targetDir.mkpath("");
+            }
+            auto descriptors = await(PatchProcess::openDir(QDir(sourceDir)));
+            if (descriptors.isEmpty()) {
+                cout << source << " is not a proper Fortune Street directory.";
+                QCoreApplication::exit(1); return;
+            }
+            QString internalNames = parser.value(internalNamesOption);
+            QStringList internalNamesList = internalNames.split(",");
+
+            QString mapIds = parser.value(mapIdOption);
+            QVector<int> mapIdList;
+            for(auto& str : mapIds.split(","))
+                mapIdList.append(str.toInt());
+
+            bool atLeastOneMatch = false;
+            for(int i=0;i<descriptors.count();i++) {
+                auto descriptor = descriptors.at(i);
+                bool exportIt = false;
+                if (parser.isSet(allOption))
+                    exportIt = true;
+                if (internalNamesList.contains(descriptor.internalName, Qt::CaseSensitivity::CaseInsensitive))
+                    exportIt = true;
+                if (mapIdList.contains(i))
+                    exportIt = true;
+                if (exportIt) {
+                    QString targetPath(targetDir.filePath(descriptor.internalName + ".yaml"));
+                    cout << "Exporting " << descriptor.internalName << " to " << targetPath << "...\n";
+                    cout.flush();
+                    PatchProcess::exportYaml(sourceDir, targetPath, descriptor);
+                    atLeastOneMatch = true;
+                }
+            }
+            if(!atLeastOneMatch) {
+                cout << "No maps matched. Try providing --id or --name.\n";
+                cout << '\n' << parser.helpText();
+            }
+        }
     } else if (command == "import") {
+
+    } else if (command == "save") {
+
+    } else if (command == "discard") {
+
+    } else if (command == "pack") {
 
     } else {
         cout << description;
