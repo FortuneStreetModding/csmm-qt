8604bfbb0bdb1e5ee4ef205a18c6718656b47c31
Deflaktor <deflaktor@gmail.com>
2021-11-03 22:51:18 +0100

    Next revision for ShopPriceMultiplier mutator

--- lib/dolio/mutatorshoppricemultiplier.cpp
+++ lib/dolio/mutatorshoppricemultiplier.cpp
@@ -5,19 +5,19 @@
 void MutatorShopPriceMultiplier::writeAsm(QDataStream &stream, const AddressMapper &addressMapper, const QVector<MapDescriptor> &) {
     // --- Owned Shop Price Multiplier ---
     {
-        quint32 hijackAddr = addressMapper.boomStreetToStandard(0x8008ff18);
+        quint32 hijackAddr = addressMapper.boomStreetToStandard(0x8008fb48);
         quint32 procShopPriceMultiplier = allocate(writeShopPriceMultiplier(addressMapper, 0), "procShopPriceMultiplier");
         stream.device()->seek(addressMapper.toFileAddress(procShopPriceMultiplier));
         auto routineCode = writeShopPriceMultiplier(addressMapper, procShopPriceMultiplier);
         for (quint32 inst: qAsConst(routineCode)) stream << inst; // re-write the routine again since now we know where it is located in the main dol
         stream.device()->seek(addressMapper.toFileAddress(hijackAddr));
-        // lwz r30,0x18(r1)           ->  b procShopPriceMultiplier
+        // lwz r0,0x24(r1)            ->  b procShopPriceMultiplier
         stream << PowerPcAsm::b(hijackAddr, procShopPriceMultiplier);
     }
 
     // --- Unowned Shop Price Multiplier ---
-   /* {
-        quint32 hijackAddr = addressMapper.boomStreetToStandard(0x8008f79c);
+    {
+        quint32 hijackAddr = addressMapper.boomStreetToStandard(0x8008f0f4);
         quint32 procUnownedShopPriceMultiplier = allocate(writeUnownedShopPriceMultiplier(addressMapper, 0), "procUnownedShopPriceMultiplier");
         stream.device()->seek(addressMapper.toFileAddress(procUnownedShopPriceMultiplier));
         auto routineCode = writeUnownedShopPriceMultiplier(addressMapper, procUnownedShopPriceMultiplier);
@@ -25,68 +25,57 @@ void MutatorShopPriceMultiplier::writeAsm(QDataStream &stream, const AddressMapp
         stream.device()->seek(addressMapper.toFileAddress(hijackAddr));
         // stw r4,0x40(r31)             ->  b procShopPriceMultiplier
         stream << PowerPcAsm::b(hijackAddr, procUnownedShopPriceMultiplier);
-    }*/
+    }
 
+    // lwz r24,0x28(r5)                                                                    -> lwz r24,0x40(r5)
+    stream.device()->seek(addressMapper.boomToFileAddress(0x80089328)); stream << PowerPcAsm::lwz(24, 0x40, 5);
 }
 
 QVector<quint32> MutatorShopPriceMultiplier::writeShopPriceMultiplier(const AddressMapper &addressMapper, quint32 routineStartAddress) {
-    // precondition:  r3 - shop price
-    // postcondition: r3 - shop price
-
     auto getMutatorDataSubroutine = addressMapper.boomStreetToStandard(0x80412c8c);
-    auto returnAddr = addressMapper.boomStreetToStandard(0x8008ff1c);
+    auto returnAddr = addressMapper.boomStreetToStandard(0x8008fb4c);
 
     QVector<quint32> asm_;
-    asm_.append(PowerPcAsm::mr(30, 3));                                                        // |. remember shop price
     asm_.append(PowerPcAsm::li(3, ShopPriceMultiplierType));                                   // \.
     asm_.append(PowerPcAsm::bl(routineStartAddress, asm_.count(), getMutatorDataSubroutine));  // /. get mutatorData
     asm_.append(PowerPcAsm::cmpwi(3, 0));                                                      // \. if mutator != NULL
-    asm_.append(PowerPcAsm::bne(4));                                                           // /.   goto mutator
+    asm_.append(PowerPcAsm::bne(3));                                                           // /.   goto mutator
                 // vanilla
-    asm_.append(PowerPcAsm::mr(3, 30));                                                        // |. restore shop price
-    asm_.append(PowerPcAsm::lwz(30, 0x18, 1));                                                 // |. replaced opcode
+    asm_.append(PowerPcAsm::lwz(0, 0x24, 1));                                                  // |. replaced opcode
     asm_.append(PowerPcAsm::b(routineStartAddress, asm_.count(), returnAddr));                 // |. return
                 // mutator stuff
-    asm_.append(PowerPcAsm::mr(4, 30));                                                        // |. restore shop price
-    asm_.append(PowerPcAsm::lhz(29, 0x0, 3));                                                  // |. r29 <- numerator
-    asm_.append(PowerPcAsm::mullw(4, 4, 29));                                                  // |. r4 <- r4 * r29
-    asm_.append(PowerPcAsm::lhz(29, 0x2, 3));                                                  // |. r29 <- denominator
-    asm_.append(PowerPcAsm::divwu(4, 4, 29));                                                  // |. r4 <- r4 / r29
-    asm_.append(PowerPcAsm::mr(3, 4));                                                         // |. r3 <- r4
-    asm_.append(PowerPcAsm::lwz(30, 0x18, 1));                                                 // |. replaced opcode
+    asm_.append(PowerPcAsm::lwz(4, 0x40, 31));                                                 // |. r4 <- calculated shop price
+    asm_.append(PowerPcAsm::lhz(5, 0x0, 3));                                                   // |. r5 <- numerator
+    asm_.append(PowerPcAsm::mullw(4, 4, 5));                                                   // |. r4 <- r4 * r5
+    asm_.append(PowerPcAsm::lhz(5, 0x2, 3));                                                   // |. r5 <- denominator
+    asm_.append(PowerPcAsm::divwu(4, 4, 5));                                                   // |. r4 <- r4 / r5
+    asm_.append(PowerPcAsm::stw(4, 0x40, 31));                                                 // |. calculated shop price <- r4
+    asm_.append(PowerPcAsm::lwz(0, 0x24, 1));                                                  // |. replaced opcode
     asm_.append(PowerPcAsm::b(routineStartAddress, asm_.count(), returnAddr));                 // |. return
     return asm_;
 }
 
 QVector<quint32> MutatorShopPriceMultiplier::writeUnownedShopPriceMultiplier(const AddressMapper &addressMapper, quint32 routineStartAddress) {
-    // precondition:  r4 - unowned shop price
-    //                r5 - owner
-    // postcondition: r3 - dont care
-    //                r4 - unowned shop price
-    //                r5 - dont care
-    //                r6 - dont care
-
     auto getMutatorDataSubroutine = addressMapper.boomStreetToStandard(0x80412c8c);
-    auto returnAddr = addressMapper.boomStreetToStandard(0x8008f7a0);
 
     QVector<quint32> asm_;
-    asm_.append(PowerPcAsm::mr(6, 4));                                                         // |. r6 <- remember shop price
+    asm_.append(PowerPcAsm::mr(31, 3));                                                        // |. save place pointer
     asm_.append(PowerPcAsm::li(3, ShopPriceMultiplierType));                                   // \.
-    asm_.append(PowerPcAsm::bl(routineStartAddress, asm_.count(), getMutatorDataSubroutine));  // /. get mutatorData
+    asm_.append(PowerPcAsm::mflr(6));                                                          // |.
+    asm_.append(PowerPcAsm::bl(routineStartAddress, asm_.count(), getMutatorDataSubroutine));  // |. get mutatorData
+    asm_.append(PowerPcAsm::mtlr(6));                                                          // |.
     asm_.append(PowerPcAsm::cmpwi(3, 0));                                                      // \. if mutator != NULL
-    asm_.append(PowerPcAsm::bne(4));                                                           // /.   goto mutator
+    asm_.append(PowerPcAsm::bne(2));                                                           // /.   goto mutator
                 // vanilla
-    asm_.append(PowerPcAsm::mr(4, 6));                                                         // |. r4 <- restore shop price
-    asm_.append(PowerPcAsm::stw(4, 0x40, 31));                                                 // |. replaced opcode
-    asm_.append(PowerPcAsm::b(routineStartAddress, asm_.count(), returnAddr));                 // |. return
+    asm_.append(PowerPcAsm::blr());                                                            // |. return
                 // mutator stuff
-    asm_.append(PowerPcAsm::mr(4, 6));                                                         // |. r4 <- restore shop price
+    asm_.append(PowerPcAsm::lwz(4, 0x40, 31));                                                 // |. r4 <- calculated shop price
     asm_.append(PowerPcAsm::lhz(5, 0x0, 3));                                                   // |. r5 <- numerator
     asm_.append(PowerPcAsm::mullw(4, 4, 5));                                                   // |. r4 <- r4 * r5
     asm_.append(PowerPcAsm::lhz(5, 0x2, 3));                                                   // |. r5 <- denominator
     asm_.append(PowerPcAsm::divwu(4, 4, 5));                                                   // |. r4 <- r4 / r5
-    asm_.append(PowerPcAsm::stw(4, 0x40, 31));                                                 // |. replaced opcode
-    asm_.append(PowerPcAsm::b(routineStartAddress, asm_.count(), returnAddr));                 // |. return
+    asm_.append(PowerPcAsm::stw(4, 0x40, 31));                                                 // |. calculated shop price <- r4
+    asm_.append(PowerPcAsm::blr());                                                            // |. return
     return asm_;
 }
 
