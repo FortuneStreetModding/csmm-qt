9773bf87fab08b7976033f8cde7f6065b7cd13d5
Landmaster <python0429@gmail.com>
2022-06-06 18:10:03 -0400

    treat switch dest ids = 0 as two state boards for backwards compatibility

--- lib/mods/dolio/arbitrarynumswitchstates.cpp
+++ lib/mods/dolio/arbitrarynumswitchstates.cpp
@@ -10,16 +10,12 @@ void ArbitraryNumSwitchStates::writeAsm(QDataStream &stream, const AddressMapper
 {
     stream.device()->seek(addressMapper.boomToFileAddress(0x8007f120));
 
-    // TODO potentially replace nops with branch and free space
-
-    // cmpwi r4, 4 -> mr r5, r4
+    // cmpwi r4, 4 -> cmpwi r4, 0
+    stream << PowerPcAsm::cmpwi(4, 0);
+    stream.skipRawData(4);
+    // li r5, 2 -> mr r5, r4
     stream << PowerPcAsm::mr(5, 4);
-    // addi r6, r31, 1 -> nop
-    stream << PowerPcAsm::nop();
-    // li r5, 2 -> nop
-    stream << PowerPcAsm::nop();
-    // bne LAB_8007f134 -> nop
-    stream << PowerPcAsm::nop();
-    // li r5, 4 -> nop
-    stream << PowerPcAsm::nop();
+    stream.skipRawData(4);
+    //li r5, 4 -> li r5, 2
+    stream << PowerPcAsm::li(5, 2);
 }
