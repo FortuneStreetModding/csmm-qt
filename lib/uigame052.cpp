8f1d6a5874868d90f890ce4e6144e7723246de00
Landmaster <python0429@gmail.com>
2022-06-17 16:36:34 -0400

    widen individual standings textboxes a little more

--- lib/uigame052.cpp
+++ lib/uigame052.cpp
@@ -17,22 +17,23 @@ bool widenDistrictName(const QString &brlytFile) {
     }
 
     std::function<void(std::shared_ptr<bq::BasePane>)> traverse = [&](std::shared_ptr<bq::BasePane> cur) {
+        float enlargeBy = 40;
         if (cur->name == "cw_waku03") { // district name pane
-            cur->width = 70 + 30;
-            cur->translate.x += 30/2;
+            cur->width = 70 + enlargeBy;
+            cur->translate.x = 5 + enlargeBy/2;
         } else if (cur->name == "cw_waku04") { // 04-06 are panes to the right
-            cur->translate.x = 70 + 30;
+            cur->translate.x = 70 + enlargeBy;
         } else if (cur->name == "cw_waku05") {
-            cur->translate.x = 140 + 30;
+            cur->translate.x = 140 + enlargeBy;
         } else if (cur->name == "cw_waku06") {
-            cur->translate.x = 230 + 30;
+            cur->translate.x = 230 + enlargeBy;
         } else if (cur->name == "w_player_01_04") { // I think this and w_player_01_05 deal with interior visual elements of cw_waku03
-            cur->width = 70 + 30;
+            cur->width = 70 + enlargeBy;
         } else if (cur->name == "w_player_01_05") {
-            cur->width = 66 + 30;
+            cur->width = 66 + enlargeBy;
         } else if (cur->name.rfind("t_area_", 0) == 0) { // check if name starts with t_area_ for the textboxes
-            cur->width = 62 + 30;
-            cur->translate.x = 5 + 30/2;
+            cur->width = 62 + enlargeBy;
+            cur->translate.x = 5 + enlargeBy/2;
         }
 
         for (auto &nxt: cur->children) {
