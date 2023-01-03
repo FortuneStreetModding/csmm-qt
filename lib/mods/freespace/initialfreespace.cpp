bc2523ca937a3b0dc85c8c14389adcaf68f65e1a
Deflaktor <deflaktor@gmail.com>
2023-01-03 19:57:57 +0100

    Move default settings freespace to mapdatafreespace.cpp

--- lib/mods/freespace/initialfreespace.cpp
+++ lib/mods/freespace/initialfreespace.cpp
@@ -9,8 +9,6 @@ void InitialFreeSpace::saveFiles(const QString &root, GameInstance &gameInstance
 {
     auto &freeSpaceManager = gameInstance.freeSpaceManager();
     auto &addressMapper = gameInstance.addressMapper();
-    // Map Default Settings Table
-    freeSpaceManager.addFreeSpace(addressMapper.boomStreetToStandard(0x804363b4), addressMapper.boomStreetToStandard(0x80436a88));
     // Unused costume string table 1
     freeSpaceManager.addFreeSpace(addressMapper.boomStreetToStandard(0x8042bc78), addressMapper.boomStreetToStandard(0x8042c240));
     // Unused costume string table 2
