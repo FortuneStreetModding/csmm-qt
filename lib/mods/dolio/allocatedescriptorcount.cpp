4df9151936e75cc00f4ec57c6ddb34284b1d79be
Deflaktor <deflaktor@gmail.com>
2023-01-09 20:36:01 +0100

    Write the size of the mapDescriptor array into the main.dol

--- lib/mods/dolio/allocatedescriptorcount.cpp
+++ lib/mods/dolio/allocatedescriptorcount.cpp
@@ -13,5 +13,7 @@ void AllocateDescriptorCount::readAsm(QDataStream &stream, const AddressMapper &
 
 void AllocateDescriptorCount::writeAsm(QDataStream &stream, const AddressMapper &addressMapper, const std::vector<MapDescriptor> &mapDescriptors)
 {
-    // crab nothing to do crab
+    stream.device()->seek(addressMapper.boomToFileAddress(0x801cca30));
+    quint32 opcode = PowerPcAsm::li(3, mapDescriptors.size());
+    stream << opcode;
 }
