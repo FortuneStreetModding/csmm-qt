a7fef1d0b8755d791e12923246b84082aa6f2806
nikkiwritescode <git@nikkums.io>
2024-02-10 17:56:26 -0600

    Implement new cache preferences with QSettings

--- preferencesdialog.cpp
+++ preferencesdialog.cpp
@@ -1,6 +1,8 @@
 #include "preferencesdialog.h"
+#include "qdir.h"
+#include "qstandardpaths.h"
 #include "ui_preferencesdialog.h"
-
+#include <QFileDialog>
 #include <QSettings>
 #include "csmmmode.h"
 
@@ -20,6 +22,60 @@ PreferencesDialog::PreferencesDialog(QWidget *parent) :
     default:
         break;
     }
+
+    switch (settings.value("networkCacheMode").toInt()) {
+    case 0:
+    case 1:
+        ui->enableCacheButton->setChecked(true);
+        enableCacheSettings();
+        break;
+    case 2:
+        ui->disableCacheButton->setChecked(true);
+        disableCacheSettings();
+        break;
+    default:
+        break;
+    }
+
+    auto size = settings.value("networkCacheSize").toInt();
+    if (size >= 1 && size <= 10) {
+        ui->cacheSizeSlider->setValue(size);
+        ui->cacheSizeLabel->setText(QString::number(size) + "GB");
+    } else {
+        ui->cacheSizeSlider->setValue(4);
+        ui->cacheSizeLabel->setText(QString::number(4) + "GB");
+    }
+
+    auto dirname = settings.value("networkCacheDirectory").toString();
+    if (!dirname.isEmpty()) {
+        ui->cacheDirectoryLabel->setText(dirname);
+    } else {
+        resetCacheDirectory();
+    }
+
+    connect(ui->updateCacheDirectoryButton, &QPushButton::clicked, this, [this](bool){
+        auto dirname = QFileDialog::getExistingDirectory(this, "Set CSMM Network Cache Directory", nullptr);
+        if (!dirname.isEmpty()) {
+            ui->cacheDirectoryLabel->setText(dirname);
+        }
+    });
+
+    connect(ui->resetCacheDirectoryButton, &QPushButton::clicked, this, [this](bool){
+        resetCacheDirectory();
+    });
+
+    connect(ui->enableCacheButton, &QPushButton::clicked, this, [this](bool){
+        enableCacheSettings();
+    });
+
+    connect(ui->disableCacheButton, &QPushButton::clicked, this, [this](bool){
+        disableCacheSettings();
+    });
+
+    connect(ui->cacheSizeSlider, &QSlider::sliderMoved, this, [this](int position){
+        auto size_str = QString::number(position);
+        ui->cacheSizeLabel->setText(size_str + "GB");
+    });
 }
 
 PreferencesDialog::~PreferencesDialog()
@@ -27,16 +83,56 @@ PreferencesDialog::~PreferencesDialog()
     delete ui;
 }
 
+
 void PreferencesDialog::accept()
 {
     QSettings settings;
-    auto checkedBtn = ui->csmmMode->checkedButton();
-    if (checkedBtn == ui->csmmModeQuickSetupButton) {
+    // default csmm mode
+    auto csmmModeBtn = ui->csmmModeBtnGroup->checkedButton();
+    if (csmmModeBtn == ui->csmmModeQuickSetupButton) {
         settings.setValue("csmmMode", EXPRESS);
-    } else if (checkedBtn == ui->csmmModeAdvancedButton) {
+    } else if (csmmModeBtn == ui->csmmModeAdvancedButton) {
         settings.setValue("csmmMode", ADVANCED);
     } else {
         settings.setValue("csmmMode", INDETERMINATE);
     }
+
+    // network cache enable/disable
+    auto networkCacheBtn = ui->cacheModeBtnGroup->checkedButton();
+    if (networkCacheBtn == ui->enableCacheButton) {
+        settings.setValue("networkCacheMode", 1);
+    } else if (networkCacheBtn == ui->disableCacheButton) {
+        settings.setValue("networkCacheMode", 2);
+    } else { // enable if not set
+        settings.setValue("networkCacheMode", 1);
+    }
+
+    auto networkCacheSize = ui->cacheSizeSlider->value();
+    settings.setValue("networkCacheSize", networkCacheSize);
+
+    auto networkCacheDirectory = ui->cacheDirectoryLabel->text();
+    settings.setValue("networkCacheDirectory", networkCacheDirectory);
+
     QDialog::accept();
+}
+
+void PreferencesDialog::enableCacheSettings()
+{
+    ui->cacheDirectoryGroupBox->setEnabled(true);
+    ui->maximumCacheSizeGroupBox->setEnabled(true);
+}
+
+
+void PreferencesDialog::disableCacheSettings()
+{
+    ui->cacheDirectoryGroupBox->setDisabled(true);
+    ui->maximumCacheSizeGroupBox->setDisabled(true);
+}
+
+
+void PreferencesDialog::resetCacheDirectory()
+{
+    QDir applicationCacheDir(QStandardPaths::writableLocation(QStandardPaths::CacheLocation));
+    auto defaultNetworkCacheDir = applicationCacheDir.filePath("networkCache");
+    ui->cacheDirectoryLabel->setText(defaultNetworkCacheDir);
 }
